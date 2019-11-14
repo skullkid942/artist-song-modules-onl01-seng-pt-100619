ebda1788c9a6eacc9ab93a30d32332a98fc72b15
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-14 16:58:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,10 +9,7 @@ class Artist
 
   @@artists = []
 
-  #def initialize
-  #  self.class.all << self
-  #  @songs = []
-  #end
+  
 
   #def self.find_by_name(name)
    # @@artists.detect{|a| a.name == name}
