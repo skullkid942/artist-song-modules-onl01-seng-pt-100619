bed604d6286203ceb2db9d70b19333c746d20ac1
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-14 16:02:47 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,10 @@
 module Memorable
-  def self.reset_all
-    self.all.clear
+  def reset_all
+    all.clear
   end
   
-  def self.count
-    self.all.count
+  def count
+    all.count
   end
   
 end 
