ca2b279e4dc1fae67012725f5b0eef458f56f9b0
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-14 16:15:56 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,13 +1,13 @@
 module Findable
   module FindSong
     def Song.find_by_name(name)
-      Song.all.detect{|a| a.name == name}
+      all.detect{|a| a.name == name}
     end
   end 
   
   module FindArtist
     def Artist.find_by_name(name)
-      Artists.all.detect{|a| a.name == name}
+      all.detect{|a| a.name == name}
     end
   end
   
