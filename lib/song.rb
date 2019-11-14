169d04e71b0ef11ed668b847ec3fd88a63230931
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-14 16:59:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,31 +10,13 @@ class Song
 
   @@songs = []
 
- # def initialize
- #  self.class.all << self
- # end
-
-  #def self.find_by_name(name)
-   # @@songs.detect{|a| a.name == name}
-  #end
 
   def self.all
     @@songs
   end
 
- # def self.reset_all
-   # self.all.clear
-  #end
-
-  #def self.count
-  #  self.all.count
- # end
-
   def artist=(artist)
     @artist = artist
   end
 
-  #def to_param
-  #  name.downcase.gsub(' ', '-')
- # end
 end
